129d695d689dc669096cdc5b73fb0247bde925c4
Josiah Chan <horming8@mbp-jiraiya.lan>
2020-10-11 09:28:18 -0700

    demonstrate using configuration and bean xml file to inject ChuckNorrisQuotes class

--- src/main/java/demo/springframework/chunkjokes/ChunkJokesApplication.java
+++ src/main/java/demo/springframework/chunkjokes/ChunkJokesApplication.java
@@ -2,8 +2,10 @@ package demo.springframework.chunkjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChunkJokesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/demo/springframework/chunkjokes/repository/ChuckNorrisQuotesRepository.java
+++ src/main/java/demo/springframework/chunkjokes/repository/ChuckNorrisQuotesRepository.java
@@ -5,14 +5,20 @@ import org.springframework.stereotype.Repository;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 @Repository
-public class ChuckNorrisQuotesRepository extends ChuckNorrisQuotes implements QuoteRepository {
+public class ChuckNorrisQuotesRepository implements QuoteRepository {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckNorrisQuotesRepository(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
 
     public String name() {
         return "Chuck Norris";
     }
 
     public String getQuote() {
-        return getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 
 }
